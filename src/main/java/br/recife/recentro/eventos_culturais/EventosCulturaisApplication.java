c59d0b2d2593a96b50fc104bcb3d036b7100a865
Fernanda Gabrielli <fernandagabriellidev@gmail.com>
2025-05-27 21:43:08 -0300

    first commit

---
+++ src/main/java/br/recife/recentro/eventos_culturais/EventosCulturaisApplication.java
@@ -0,0 +1,11 @@
+package br.recife.recentro.eventos_culturais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosCulturaisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventosCulturaisApplication.class, args);
+    }
+}
---
+++ src/test/java/br/recife/recentro/eventos_culturais/EventosCulturaisApplicationTests.java
@@ -0,0 +1,11 @@
+package br.recife.recentro.eventos_culturais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosCulturaisApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
